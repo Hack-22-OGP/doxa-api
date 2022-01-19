d6e029da6a6900c6ce1939c0673edf41a4861f66
silentcoder <hariharan@mom.gov.sg>
2022-01-19 17:16:52 +0800

    Add cors header under helper

--- src/helpers/http-format.js
+++ src/helpers/http-format.js
@@ -1,6 +1,10 @@
 const formatHttpResponse = (success, response) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify({
       success: success,
       response: response,
